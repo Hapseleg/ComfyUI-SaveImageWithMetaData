48d0ce283128a626a20f611ce74fe86ee708b589
Hapse <nicolajljo@gmail.com>
2025-11-22 16:56:10 +0100

    ghetto fork
    https://github.com/comfy-nodes/ComfyUI-SaveImageWithMetaData/blob/193605935f977ed8747c6513a250afbb6f4030cf/py/__init__.py#L25-L69

--- __init__.py
+++ __init__.py
@@ -1,13 +1,69 @@
-from .py.nodes.node import SaveImageWithMetaData, CreateExtraMetaData
+import functools
 
-NODE_CLASS_MAPPINGS = {
-    "SaveImageWithMetaData": SaveImageWithMetaData,
-    "CreateExtraMetaData": CreateExtraMetaData,
-}
+from .hook import pre_execute, pre_get_input_data
+import execution
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "SaveImageWithMetaData": "Save Image With Metadata",
-    "CreateExtraMetaData": "Create Extra MetaData",
-}
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+# refer. https://stackoverflow.com/a/35758398
+def prefix_function(function, prefunction):
+    @functools.wraps(function)
+    def run(*args, **kwargs):
+        prefunction(*args, **kwargs)
+        return function(*args, **kwargs)
+
+    return run
+
+
+execution.PromptExecutor.execute = prefix_function(
+    execution.PromptExecutor.execute, pre_execute
+)
+
+
+execution.get_input_data = prefix_function(execution.get_input_data, pre_get_input_data)
+
+
+def get_output_cache_wrapper(self, input_unique_id, unique_id):
+    return self.output_cache.get(input_unique_id)
+
+from comfy_execution.graph import ExecutionList
+ExecutionList.get_output_cache = get_output_cache_wrapper
+
+
+# --- Compatibility shims for ComfyUI cache objects ---
+# Make HierarchicalCache/LRUCache/NullCache expose get_output_cache(...) expected by execution.get_input_data(...)
+from comfy_execution.caching import HierarchicalCache, LRUCache, NullCache
+
+def _cache_get_output_cache(self, input_unique_id, unique_id):
+    # unique_id is ignored here; we only need cached outputs of input_unique_id
+    try:
+        return self.get(input_unique_id)
+    except Exception:
+        return None
+
+def _nullcache_get_output_cache(self, input_unique_id, unique_id):
+    return None
+
+if not hasattr(HierarchicalCache, "get_output_cache"):
+    HierarchicalCache.get_output_cache = _cache_get_output_cache
+if not hasattr(LRUCache, "get_output_cache"):
+    LRUCache.get_output_cache = _cache_get_output_cache
+if not hasattr(NullCache, "get_output_cache"):
+    NullCache.get_output_cache = _nullcache_get_output_cache
+
+
+def _cache_get_cache(self, input_unique_id, unique_id):
+    # unique_id is ignored here; we only need cached outputs of input_unique_id
+    try:
+        return self.get(input_unique_id)
+    except Exception:
+        return None
+
+def _nullcache_get_cache(self, input_unique_id, unique_id):
+    return None
+
+if not hasattr(HierarchicalCache, "get_cache"):
+    HierarchicalCache.get_cache = _cache_get_cache
+if not hasattr(LRUCache, "get_cache"):
+    LRUCache.get_cache = _cache_get_cache
+if not hasattr(NullCache, "get_cache"):
+    NullCache.get_cache = _nullcache_get_cache
